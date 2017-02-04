14cf6f1169566ed2355fe4f52109641ba2c42392
Michael Liu <michael.liu95@gmail.com>
2017-02-03 20:11:25 -0500

    Setting up husband email

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 var hostageApp = angular.module('hostageApp', []);
 
-hostageApp.controller('HostageController', function HostageController($scope) {
+hostageApp.controller('HostageController', function HostageController($scope, $timeout) {
   $scope.emails = [
-    {from: "MommyJones@geemail.com", subject: "Fwd: You will not believe this! WOW!", time: "7:10pm", read: false}
+    {from: "Wifey@geemail.com", subject: "Honeymoon photos!", time: moment().format("h:mma"), read: false, text: "Hey babe ;) Photos just came in! Love you 4evr <3 <3", image: "honeymoon.png", trigger: honeymoonEmail},
+    {from: "MommyJones@geemail.com", subject: "Fwd: You will not believe this! WOW!", time: "7:10pm", read: true, text: "Check this out!", image: "poop.jpg", trigger: null}
   ];
 
   $scope.selectedEmail = null;
@@ -10,5 +11,39 @@ hostageApp.controller('HostageController', function HostageController($scope) {
   $scope.openEmail = function(email) {
     email.read = true;
     $scope.selectedEmail = email;
+    if (email.trigger != null) {
+      email.trigger();
+    }
+  }
+
+  var honeymoonEmailRan = false;
+  function honeymoonEmail() {
+    if (honeymoonEmailRan) {
+      return;
+    }
+    honeymoonEmailRan = true;
+
+    // Send first hostage email 23 seconds after
+    $timeout(function() {
+      $scope.emails.unshift(
+      {
+        from: "UNKNOWN EMAIL",
+        subject: "No subject",
+        time: moment().format("h:mma"),
+        read: false,
+        text: "",
+        image: "haveHusband.png",
+        trigger: haveHusbandEmail
+      });
+    }, 1000);
+  }
+
+  var haveHusbandEmailRan = false;
+  function haveHusbandEmail() {
+    if (haveHusbandEmailRan) {
+      return;
+    }
+    haveHusbandEmailRan = true;
+
   }
 });
